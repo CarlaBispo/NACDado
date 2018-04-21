f33c8c680ed7c02de0b11b76666a6eb69610acfc
logonrm <logonrm@fiap.com.br>
2018-04-20 22:57:49 -0300

    Initial commit

---
+++ app/src/main/java/sales/sayuri/com/nacdado/GameActivity.kt
@@ -0,0 +1,58 @@
+package sales.sayuri.com.nacdado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_game.*
+import java.util.*
+
+class GameActivity : AppCompatActivity() {
+
+    var vitorias = 0
+    var derrotas = 0
+    var empates = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        btPlay.setOnClickListener{
+            jogar()
+        }
+
+    }
+
+    fun jogar(){
+        val gerador = Random()
+        val numeroPlayer = gerador.nextInt(6) + 1
+        val numeroPC = gerador.nextInt(6) + 1
+
+        configuraJogada(numeroPlayer, ivPlayer)
+        configuraJogada(numeroPC, ivPC)
+
+        if (numeroPlayer < numeroPC){
+            derrotas++
+            tvLoses.text = derrotas.toString()
+        }else if (numeroPlayer > numeroPC) {
+            vitorias++
+            tvWins.text = vitorias.toString()
+        }else {
+            empates
+            tvDraws.text = empates.toString()
+        }
+
+    }
+    fun configuraJogada (numero: Int, imageView: ImageView){
+        when(numero) {
+            1-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_1))
+            2-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_2))
+            3-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_3))
+            4-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_4))
+            5-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_5))
+            6-> imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dice_6))
+        }
+    }
+
+
+}
